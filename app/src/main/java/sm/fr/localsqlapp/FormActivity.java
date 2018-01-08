09d5117f96862c7f6f4ab8d0b91c15f98f7ed6a1
formation <formation@mail.fr>
2018-01-08 10:36:15 +0100

    Ajout d'une activiter FormActivity pour pour ajouter un contact

--- app/src/main/java/sm/fr/localsqlapp/FormActivity.java
+++ app/src/main/java/sm/fr/localsqlapp/FormActivity.java
@@ -2,6 +2,8 @@ package sm.fr.localsqlapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_activity);
+    }
+
+    public void onValid(View v){
+        Button clikedButton = (Button) v;
     }
 }
