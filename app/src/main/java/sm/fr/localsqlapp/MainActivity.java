c6930fd90486ce551961bdff8cc4e0330d091e3f
formation <formation@mail.fr>
2018-01-08 16:53:23 +0100

    Lecture base de données

--- app/src/main/java/sm/fr/localsqlapp/MainActivity.java
+++ app/src/main/java/sm/fr/localsqlapp/MainActivity.java
@@ -1,9 +1,17 @@
 package sm.fr.localsqlapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import sm.fr.localsqlapp.database.DatabaseHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +29,25 @@ public class MainActivity extends AppCompatActivity {
     public void onAddContact(View view) {
         Intent FormIntent = new Intent(this, FormActivity.class);
         startActivity(FormIntent);
+    }
+    private List<Map<String,String>> getAllContatcts(){
+        //instanciation de la connexion à la base de données
+        DatabaseHandler db= new DatabaseHandler(this);
+
+        //Exécution de la requête de selection
+        Cursor cursor= db.getReadableDatabase().rawQuery("SELECT name,first_name,email FROM contacts",null);
+        //Instenciation de la ligne qui renvera les données.
+        List<Map<String ,String>>contactList = new ArrayList<>();
+        Map<String ,String> contactCols= new HashMap<>();
+        //Parcours du curseur
+        while (cursor.moveToNext()){
+            //remplissage du tableau associatif en fonction des données du curseur
+            contactCols.put("name",cursor.getString(0));
+            contactCols.put("first_name",cursor.getString(1));
+            contactCols.put("email",cursor.getString(2));
+            //Ajout du Map à la liste
+            contactList.add(contactCols);
+        }
+        return contactList;
     }
 }
