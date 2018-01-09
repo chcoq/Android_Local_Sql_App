fd66550ed5b66a8b141b0519ee8f1f032db6b3e4
formation <formation@mail.fr>
2018-01-09 14:05:23 +0100

    Gestion du Menu sur main activity

--- app/src/main/java/sm/fr/localsqlapp/MainActivity.java
+++ app/src/main/java/sm/fr/localsqlapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -18,9 +20,9 @@ import sm.fr.localsqlapp.database.DatabaseHandler;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private ListView contactListView;
-    private List<Map<String,String>>contactList;
+    private List<Map<String, String>> contactList;
     private Integer selectedIndex;
-    private Map<String,String>selectedPerson;
+    private Map<String, String> selectedPerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         //Référence au widget ListView sur le layout
         ListView contactListView = findViewById(R.id.contactListView);
         //Récupération  de la  ligne des contacts
-        contactList= this.getAllContatcts();
+        contactList = this.getAllContatcts();
         //Création d'un contactArrayAdapter
         ContactArrayAdapter contatctAdapter = new ContactArrayAdapter(this, contactList);
         //Définition de l'adapter de notre listView
@@ -38,6 +40,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         //Définition d'un écouteur d'événement pour
         contactListView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //ajout des entrées du fichier mainOptionMenu
+        //au menu contextuel de l'activité
+        getMenuInflater().inflate(R.menu.main_option_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.mainMenuOptionDelete:
+
+                break;
+            case R.id.mainMenuOptionEdit:
+
+                break;
+        }
+
+        return true;
     }
 
     /**
@@ -49,22 +75,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Intent FormIntent = new Intent(this, FormActivity.class);
         startActivity(FormIntent);
     }
-    private List<Map<String,String>> getAllContatcts(){
+
+    private List<Map<String, String>> getAllContatcts() {
         //instanciation de la connexion à la base de données
-        DatabaseHandler db= new DatabaseHandler(this);
+        DatabaseHandler db = new DatabaseHandler(this);
 
         //Exécution de la requête de selection
-        Cursor cursor= db.getReadableDatabase().rawQuery("SELECT name,first_name,email FROM contacts",null);
+        Cursor cursor = db.getReadableDatabase().rawQuery("SELECT name,first_name,email FROM contacts", null);
         //Instenciation de la ligne qui renvera les données.
-        List<Map<String ,String>>contactList = new ArrayList<>();
+        List<Map<String, String>> contactList = new ArrayList<>();
 
         //Parcours du curseur
-        while (cursor.moveToNext()){
-            Map<String ,String> contactCols= new HashMap<>();
+        while (cursor.moveToNext()) {
+            Map<String, String> contactCols = new HashMap<>();
             //remplissage du tableau associatif en fonction des données du curseur
-            contactCols.put("name",cursor.getString(0));
-            contactCols.put("first_name",cursor.getString(1));
-            contactCols.put("email",cursor.getString(2));
+            contactCols.put("name", cursor.getString(0));
+            contactCols.put("first_name", cursor.getString(1));
+            contactCols.put("email", cursor.getString(2));
             //Ajout du Map à la liste
             contactList.add(contactCols);
         }
@@ -74,8 +101,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        this.selectedIndex= i;
+        this.selectedIndex = i;
         this.selectedPerson = contactList.get(i);
-        Toast.makeText(this,"ligne"+i +"cliquer",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "ligne" + i + "cliquer", Toast.LENGTH_SHORT).show();
     }
 }
